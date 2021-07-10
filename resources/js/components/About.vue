c8752c51bae2d1e8b94d7f7e8b150e765b2d75f5
Nabeel Ahmed <nabeel@eteamid.com>
2021-07-10 16:22:36 +0500

    Initial commit

---
+++ resources/js/components/About.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container">
+        <h1>Hello From About Component</h1>
+        <router-link to="/">Switch To Home</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "About"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container">
+        <h1>Hello From Home Component</h1>
+        <router-link to="/about">Switch To About</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home"
+    }
+</script>
+
+<style scoped>
+
+</style>
